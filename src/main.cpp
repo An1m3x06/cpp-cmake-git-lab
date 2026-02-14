36b41ac5b158e6d3c569165560508fe293214ef9
Blah-Blah <farakhovn1@gmail.com>
2026-02-14 17:30:26 +0300

    Print Message B

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "math.hpp"
 
 int main() {
-    std::cout << "Hello, GitHub + CMake!" << std::endl;
+    std::cout << "Message B" << std::endl;
     std::cout << "2 + 3 = " << add(2, 3) << std::endl;
     return 0;
 }
