23c002083a16d600f53f7d971cdde3d2465596dc
Blah-Blah <farakhovn1@gmail.com>
2026-02-14 17:30:18 +0300

    Use add() in main output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "math.hpp"
 
 int main() {
     std::cout << "Hello, GitHub + CMake!" << std::endl;
+    std::cout << "2 + 3 = " << add(2, 3) << std::endl;
     return 0;
 }
